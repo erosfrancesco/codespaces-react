a68fa86b5ebf80713be468869cef31e6b47d845a
eros francesco reale <eros.francesco.francesco@gmail.com>
2026-06-02 14:19:28 +0000

    set up proper icons and code clean

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
-import './App.css';
 import { WebSocketProvider } from './hooks/useWebSocket';
 import { Dashboard } from './sections/Dashboard';
 
 function App() {
   return (
-    <div className="App">
-      <WebSocketProvider>
-        <Dashboard />
-      </WebSocketProvider>
-    </div>
+    <WebSocketProvider>
+      <Dashboard />
+    </WebSocketProvider>
   );
 }
 
